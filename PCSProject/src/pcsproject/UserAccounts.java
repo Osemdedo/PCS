2eeaf893bba6b99aa0b7ab0ec6037c3c6cf85ed3
Osemdedo <mf.costa@live.com>
2015-10-24 16:22:40 -0200

    Final

--- PCSProject/src/pcsproject/UserAccounts.java
+++ PCSProject/src/pcsproject/UserAccounts.java
@@ -1,8 +1,4 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package pcsproject;
 
 import java.io.BufferedReader;
@@ -15,12 +11,30 @@ import java.util.List;
 import java.util.Map;
 import java.io.*;
 
+/*
+    Esta classe tem como função manipular um arquivo de texto(txt) para gerenciar os registros de todos 
+    os jogadores.
+    Esta função é realizada por uma estrutura de tags no documento(arquivo.txt) e por uma estrutura de 
+    dicionarios na classe UserAccounts.
+    Tal estrutura de Dicionario tem a seguinte caracteristica:
+    Map<String, Map<String,String> >
+    Onde a sua key é o nome do usuário e leva a outro dicionário
+    Neste outro dicionário tem-se como key a tag que foi definida no file e sua definição como conteúdo.
 
+    O Documento é formado por tag, qualquer tag é aceitavel, porém algumas regas devem ser tomadas
+    Regras:
+    A primeira tag de um usuário deve ser seu nome e depois de todas as tags 
+    a string ">>>" deve ser adicionada para a separacção dos elementos
+
+*/
 public class UserAccounts {
     
+    // armazena todos os usuarios cadastrados
+    // é definido na função LoadAllUsers
     static Map<String, Map<String,String> > allUsers;
             
 
+    // Le o TxT e retorna uma lista de String contendo todas as linhas fo arquivo 
     public static List<String> ReadFile() {
       
         String fileName = "arquivo.txt";
@@ -49,7 +63,9 @@ public class UserAccounts {
     }
     
     
-    
+    // essa função deve ser chamada quando se quer Adicionar um novo Usuário ou Editar um 
+    //usuário existente.
+    // Recebe um dicionario com todas as caracteristicas 
     public static void ChangeArquivo(Map<String,String> userdata){
         if(allUsers.containsKey(userdata)){
             AddUser(userdata);
@@ -59,9 +75,10 @@ public class UserAccounts {
         }
     }
     
+    //Recebe o dicionario com os dados do usuario e insere no documento
+    //Ao editar um usuario, ele é removido de sua posição e adicionado no final com os novos valores
     public static void EditUser(Map<String,String> userdata){
-       
-        
+    
         
         File fileName = new File("arquivo.txt");
         File tempFile = new File("temp.txt");
@@ -76,19 +93,15 @@ public class UserAccounts {
             FileReader fileReader = new FileReader(fileName);
             BufferedReader bufferedReader = new BufferedReader(fileReader);
             BufferedWriter bw = new BufferedWriter(fileWriter);
-            
-            
-            
+        
             while((line = bufferedReader.readLine()) != null) {
-                
-                
+         
                 if(line.startsWith("nome")){
                     splitLine = line.split(":");
                     if(splitLine[1].equals(userdata.get("nome"))){
                         
                         System.out.println("wooow");
-                        aux=1;
-                        
+                        aux=1;               
                     }
                         
                 }
@@ -108,13 +121,8 @@ public class UserAccounts {
                     //System.out.println(line);
                     bw.write(line);
                     bw.newLine();
-                }
-                
-                
-                    
-            }           
-             
-            
+                }              
+            }                  
             
             bw.close();
             bufferedReader.close();
@@ -133,6 +141,7 @@ public class UserAccounts {
        
     }
     
+    //Recebe os dados do usuria por um dicionário e adiciona-o no final do documento
     public static void AddUser(Map<String,String> userdata){
     
         
@@ -166,43 +175,34 @@ public class UserAccounts {
             ioe.printStackTrace();
        }
     
-        
-    
-    
-    
     }
     
-    
+    //Carrega para o Dicinonario allUsers todos os usuario do file
     public static void LoadAllUsers(){
         
              Map<String, Map<String,String> > map = new HashMap<String, Map<String,String>>();
              Map<String,String> user = new HashMap<String, String>();
-             
-             
-             
+                       
              List<String> lines = ReadFile();
              int found =0;
              String[] splitedString;
              for( String line: lines)  
             {  
-                 
-                
+                           
                 if(line.contains(">>>")){
                     String nome = user.get("nome");
-                    map.put(nome, user);
-    
+                    map.put(nome, user);  
                 } 
+                
                 else{
                     splitedString = line.split(":");   
-                    user.put(splitedString[0],splitedString[1]);  
-         
+                    user.put(splitedString[0],splitedString[1]);        
                 }
             }
-             allUsers = map;
-             
+             allUsers = map;            
     }
-    
-    
+      
+    //Retorna o dicionario com todos os usuarios
     public  static Map<String, Map<String,String> > GetallUsers(){
         return allUsers;
     }
